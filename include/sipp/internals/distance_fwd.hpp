b5b085612458b8068cad94a6d1c1f1cfebbeac14
Viacheslav Kalmykov <slava@kalmykovs.com>
2017-12-29 14:52:59 +0100

    Added missing <ratio> includes

--- include/sipp/internals/distance_fwd.hpp
+++ include/sipp/internals/distance_fwd.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ratio>
+
 namespace sipp {
 
 template<class Rep, class Ratio>
--- include/sipp/internals/speed_fwd.hpp
+++ include/sipp/internals/speed_fwd.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ratio>
+
 namespace sipp {
 
 template<class Rep, class DistanceType, class Ratio>
